0ed3d90393c0eae486b2853dca7139ba4dacb0e5
data-kid <gabriel.grover@gmail.com>
2016-03-03 00:06:20 -0800

    finished array element equality

--- TDD_practice/Set/lib/MySet.rb
+++ TDD_practice/Set/lib/MySet.rb
@@ -2,48 +2,54 @@ require 'byebug'
 require_relative 'SetErrors.rb'
 
 class MySet
-  
+
   attr_reader :set
-  
+
   def initialize(num)
    @set =  Array.new(num)
   end
-  
+
   def cardinality
     @set.length
   end
-  
+
   def empty?
     cardinality == 0
   end
-  
+
   def place(index, elem)
     @set[index] = elem
     if index >= cardinality || index < 0
       raise OutOfBoundsError, 'index Specified is out of bounds'
     end
   end
-  
+
   def remove(index)
     new_arr = []
     i = 0
     while i < cardinality do
-      new_arr << set[i] unless i == index 
+      new_arr << set[i] unless i == index
       i+=1
     end
     @set = new_arr
   end
-  
+
   def ==(other)
     match?(self.set, other.set)
-  end  
-  
-  private
-  
-  def match?(objA, objB)
-    objA == objB
   end
-  
-  
-  
+
+  private
+
+  def match?(objA, objB)
+    #debugger
+    if objA.is_a?(Array) && objB.is_a?(Array)
+      objA.each do |a|
+        return false if objB.find_index{ |e| match? a,e }.nil?
+      end
+    end
+    true
+  end
+
+
+
 end
--- TDD_practice/Set/test/equality_test.rb
+++ TDD_practice/Set/test/equality_test.rb
@@ -3,35 +3,60 @@ require "minitest/autorun"
 require "MySet.rb"
 
 class EqualityTest < MiniTest::Test
-  
+
   def test_equality_between_sets_without_data_structure_elements_and_order
     setA = build_set(5)
     setB = build_set(5)
     assert(setA == setB, 'Sets with identical elements should be equal')
   end
-  
+
   def test_equality_between_sets_without_data_structure_elements_ignoring_order
-    setA = MySet
-    
-    setB = build_set(5)
+    setA = MySet.new(2)
+    setB = build_set(2)
+    setA.place(0,1)
+    setA.place(1,2)
+    setB.place(0,2)
+    setB.place(1,1)
     assert(setA == setB, 'Sets with identical elements should be equal')
   end
-  
-  
+
+  def test_equality_between_sets_with_array_elements
+    setA = MySet.new(2)
+    setB = MySet.new(2)
+    setA.place(0,[2,1])
+    setA.place(1,3)
+    setB.place(0,3)
+    setB.place(1,[1,2])
+    assert(setA == setB, 'Set with identical elements shoud be equal')
+    setB.place(1,[1,0])
+    refute(setA == setB)
+  end
+
+  def test_equality_between_sets_with_hash_elements
+    setA = MySet.new(2)
+    setB = MySet.new(2)
+    setA.place(0,{one:1, two:2})
+    setA.place(1,3)
+    setB.place(0,3)
+    setB.place(1, {two:2, one:1})
+    assert(setA == setB)
+  end
+
+
   private
-  
+
     def build_set(num)
       set = MySet.new(num)
       i = 0
       j = 1
-      while i < set.cardinality - 1 do
+      while i < set.cardinality do
         set.place(i, j)
         i += 1
         j += 1
       end
-      
+
       set
-    
+
     end
-  
+
 end
