387c22b9721cfd21d86158c534843269a24cb74c
data-kid <gabriel.grover@gmail.com>
2016-09-05 08:56:11 -0700

    remove dups

--- CTCI/linked_lists/Node.rb
+++ CTCI/linked_lists/Node.rb
@@ -1,4 +1,3 @@
-require "pry"
 
 class Node
   attr_accessor :next 
@@ -20,8 +19,6 @@ class Node
     
 end
 
-
-
 def delete_node(head, data)
   return head.next if head.data == data
   n = head
@@ -36,10 +33,43 @@ def delete_node(head, data)
   head
 end
 
-def make_int_list(args)
-  head = Node.new args.shift
-  args.each do |arg|
-    head.append arg  
+def delete_node!(head, data)
+  #return head.next if head.data == data
+  n = head
+  until n.next.nil? do 
+    if n.next.data == data
+      n.next = n.next.next
+      return head
+    else
+      n = n.next
+    end
   end
   head
 end
+
+#make_int_list is for testing purposes
+
+def make_int_list(args)
+  head = Node.new args[0]
+  args[1..args.length - 1].each { |arg| head.append arg }
+  head
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CTCI/linked_lists/remove_dups.rb
+++ CTCI/linked_lists/remove_dups.rb
@@ -1,16 +1,31 @@
 require "./Node.rb"
-require "pry"
+
+# def remove_dups list 
+#   #byebug
+#   ledger = {}
+#   node = list 
+#   until node.nil? do 
+#     ledger[node.data] = node
+#     node = node.next 
+#   end
+#   
+#   1.upto(ledger.values.length) do |i|
+#     ledger.values[i - 1].next = ledger.values[i]
+#   end
+#   list
+# end
+
 def remove_dups list 
-  #byebug
-  ledger = {}
-  node = list 
-  until node.nil? do 
-    ledger[node.data] = node
-    node = node.next 
-  end
-  
-  1.upto(ledger.values.length) do |i|
-    ledger.values[i - 1].next = ledger.values[i]
-  end
-  list
+	p1 = list
+	until p1.nil? do 
+		p2 = p1
+		until p2.next.nil? do 
+			if p2.next.data == p1.data
+				p2.next = p2.next.next
+			end
+			p2 = p2.next
+		end
+		p1 = p1.next
+	end
 end
+
